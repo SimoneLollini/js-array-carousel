5b8ec55b94189043634e187a93f17066c44e0ea5
SimoneLollini <lollinisimone@gmail.com>
2022-10-12 19:21:54 +0200

    add buttons

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -19,16 +19,46 @@ for (let i = 0; i < images.length; i++) {
     const activeSlide = ` <img class="${i === activeImage ? 'active_slide' : ''}" src="./assets/${slideEl}" alt="">`
     console.log(activeSlide);
 
-    document.querySelector('.container').insertAdjacentHTML('beforeend', activeSlide);
+    document.querySelector('.carousel').insertAdjacentHTML('beforeend', activeSlide);
     // document.querySelector(".container>img").style.display = "none";
     // document.querySelector('.container').insertAdjacentHTML('afterbegin', slideEL)
     // document.querySelector('.carousel > img').style.display = "inline-block";
-
 }
 
-// se la condizione active slide= slideEl è vera allora abbino class active
-
-// .classList.add("active");
 
 //  Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
 // Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1
+
+const nextButtonEl = document.querySelector('.next');
+const prevButtonEl = document.querySelector('.prev');
+
+nextButtonEl.addEventListener('click', function () {
+    // console.log('next');
+    const slides = document.querySelectorAll('.carousel > img'); // getElementsByClassName
+    const currentSlide = slides[activeImage]; // slides[0]
+    // tolgo classe active
+    currentSlide.classList.remove('active_slide')
+    // incremento active image di uno
+    activeImage++
+    //seleziono la seconda immagine
+    const nextSlide = slides[activeImage]
+    console.log(nextSlide);
+    // gli applico la classe active
+    nextSlide.classList.add('active_slide')
+
+})
+prevButtonEl.addEventListener('click', function () {
+    // console.log('prev');
+    const slides = document.querySelectorAll('.carousel > img'); // getElementsByClassName
+    const currentSlide = slides[activeImage]; // slides[0]
+    // tolgo classe active
+    currentSlide.classList.remove('active_slide')
+    // incremento active image di uno
+    activeImage--
+    //seleziono la seconda immagine
+    const nextSlide = slides[activeImage]
+    console.log(nextSlide);
+    // gli applico la classe active
+    nextSlide.classList.add('active_slide')
+
+})
