fc1dfa6749581f366d8c59daecacebd4e7fd2d66
SimoneLollini <lollinisimone@gmail.com>
2022-10-12 17:43:35 +0200

    fix array

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -12,17 +12,16 @@ for (let i = 0; i < images.length; i++) {
     const slideEl = images[i];
     const activeSlide = ` <img src="./assets/${slideEl}" alt="">`
     console.log(activeSlide);
-    document.querySelector('.container').insertAdjacentHTML('beforeend', activeSlide)
+    // document.querySelector(".container>img").style.display = "none";
     // document.querySelector('.container').insertAdjacentHTML('afterbegin', slideEL)
     // document.querySelector('.carousel > img').style.display = "none";
     // document.querySelector('.carousel > img').style.display = "inline-block";
-
+    document.querySelector('.container').insertAdjacentHTML('beforeend', activeSlide)
 }
 
-
-// document.querySelector('.carousel > img').classList.add("active");
+// se la condizione active slide= slideEl è vera allora abbino class active
 
 // .classList.add("active");
-// document.querySelector('.container').insertAdjacentHTML('afterbegin', slideArray)
+
 //  Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
 // Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1
